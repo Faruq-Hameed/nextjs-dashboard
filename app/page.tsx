1d1dffd7a1f6d114e5204d7acaebd1204de31c5d
Faruq Hameed <108727947+Faruq-Hameed@users.noreply.github.com>
2025-12-30 10:42:32 +0100

    Refactor page component: reorganize imports and enhance image handling for responsive design

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import styles from '@/app/ui/home.module.css'
+import Image from "next/image";
+
+import AcmeLogo from "@/app/ui/acme-logo";
+import styles from "@/app/ui/home.module.css";
 import { lusitana } from "./ui/fonts";
 
 export default function Page() {
@@ -14,7 +16,9 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <div className={styles.shape} />
           {/* <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" /> */}
-          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal `}>
+          <p
+            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal `}
+          >
             <strong>Welcome to Acme.</strong> This is the example for the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -28,8 +32,24 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
+
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          {/* Larger image */}
+          <Image
+            src="/hero-desktop.png"
+            alt="Screenshots of the dashboard project showing desktop version"
+            width={1000} //this are not the rendering size but just to show original image sizes
+            height={700}
+            className="hidden md:block" //hidden by defaults i.e emove the image from the DOM on mobile screens but on medium and above layout is block
+          />
+          {/* Small sreens image */}
+          <Image
+          src="/hero-mobile.png"
+          alt="Screenshots of the dashboard project showing mobile version"
+          width={560}
+          height={620}
+          className="block md:hidden" //default i.e small screen is block while others hidden
+          />
         </div>
       </div>
     </main>
